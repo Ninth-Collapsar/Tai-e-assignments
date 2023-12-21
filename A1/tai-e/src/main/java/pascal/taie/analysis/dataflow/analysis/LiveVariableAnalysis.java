f29759a9e193e3afdba5beb0854c50a3c4da77de
Ninth-Collapsar <110378474+Ninth-Collapsar@users.noreply.github.com>
2023-12-21 11:45:25 +0800

    Add files via upload

--- A1/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/LiveVariableAnalysis.java
+++ A1/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/LiveVariableAnalysis.java
@@ -25,8 +25,13 @@ package pascal.taie.analysis.dataflow.analysis;
 import pascal.taie.analysis.dataflow.fact.SetFact;
 import pascal.taie.analysis.graph.cfg.CFG;
 import pascal.taie.config.AnalysisConfig;
+import pascal.taie.ir.exp.LValue;
+import pascal.taie.ir.exp.RValue;
 import pascal.taie.ir.exp.Var;
 import pascal.taie.ir.stmt.Stmt;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Implementation of classic live variable analysis.
@@ -48,23 +53,48 @@ public class LiveVariableAnalysis extends
     @Override
     public SetFact<Var> newBoundaryFact(CFG<Stmt> cfg) {
         // TODO - finish me
-        return null;
+        // return null;
+        return new SetFact<>();
     }
 
     @Override
     public SetFact<Var> newInitialFact() {
         // TODO - finish me
-        return null;
+        // return null;
+        return new SetFact<>();
     }
 
     @Override
     public void meetInto(SetFact<Var> fact, SetFact<Var> target) {
         // TODO - finish me
+        target.union(fact);
     }
 
     @Override
     public boolean transferNode(Stmt stmt, SetFact<Var> in, SetFact<Var> out) {
         // TODO - finish me
-        return false;
+        Optional<LValue> def = stmt.getDef();
+        List<RValue> use = stmt.getUses();
+
+        // the {SetFact<Var> in} is excepted to be changed, rather than a {newIN}.
+        SetFact<Var> oldIn = in.copy();
+        in.set(out);
+
+        if (def.isPresent()) {
+            LValue lv = def.get();
+            if (lv.getClass() == Var.class) {
+                Var newLV = (Var) lv;
+                in.remove(newLV);
+            }
+        }
+
+        for (RValue rv: use) {
+            if (rv.getClass() == Var.class) {
+                Var newRV = (Var) rv;
+                in.add(newRV);
+            }
+        }
+
+        return !oldIn.equals(in);
     }
 }

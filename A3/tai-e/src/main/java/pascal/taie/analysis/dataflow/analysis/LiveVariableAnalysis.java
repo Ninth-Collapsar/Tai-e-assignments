fc1cb329f5cb657f8b91d5794517a2b90102d6fd
Ninth-Collapsar <110378474+Ninth-Collapsar@users.noreply.github.com>
2023-12-21 12:00:15 +0800

    Add files via upload

--- A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/LiveVariableAnalysis.java
+++ A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/LiveVariableAnalysis.java
@@ -25,8 +25,13 @@ package pascal.taie.analysis.dataflow.analysis;
 import pascal.taie.analysis.dataflow.fact.SetFact;
 import pascal.taie.analysis.graph.cfg.CFG;
 import pascal.taie.config.AnalysisConfig;
+import pascal.taie.ir.exp.LValue;
+import pascal.taie.ir.exp.RValue;
 import pascal.taie.ir.exp.Var;
 import pascal.taie.ir.stmt.Stmt;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Implementation of classic live variable analysis.
@@ -47,24 +52,38 @@ public class LiveVariableAnalysis extends
 
     @Override
     public SetFact<Var> newBoundaryFact(CFG<Stmt> cfg) {
-        // TODO - finish me
-        return null;
+        return new SetFact<>();
     }
 
     @Override
     public SetFact<Var> newInitialFact() {
-        // TODO - finish me
-        return null;
+        return new SetFact<>();
     }
 
     @Override
     public void meetInto(SetFact<Var> fact, SetFact<Var> target) {
-        // TODO - finish me
+        target.union(fact);
     }
 
     @Override
     public boolean transferNode(Stmt stmt, SetFact<Var> in, SetFact<Var> out) {
-        // TODO - finish me
-        return false;
+        SetFact<Var> oldIn = in.copy();
+        in.set(out);
+
+        Optional<LValue> lv = stmt.getDef();
+        List<RValue> rv = stmt.getUses();
+
+        if (lv.isPresent() && lv.get().getClass() == Var.class) {
+            in.remove((Var) lv.get());
+        }
+
+        for (RValue r : rv) {
+            if (r.getClass() == Var.class) {
+                in.add((Var) r);
+            }
+        }
+
+        return !in.equals(oldIn);
     }
+
 }
